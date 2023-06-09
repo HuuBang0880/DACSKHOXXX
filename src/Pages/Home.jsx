19da383af6d5c75b2be2d48f95ff86fe5471081e
HuuBang0880 <azzaro98aa@gmail.com>
2023-06-09 14:48:50 +0700

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SectionHeader, { SectionTittle } from "../components/Home/SectionHeader";
 import Slider, { SliderCoop } from "../components/Slider/Slider";
 import images from "../access/Images";
-import ImageTitle, { ImageTuyenSinh } from "../components/Home/ImageTitle";
+import ImageTitle, { HomeButtons, ImageTuyenSinh } from "../components/Home/ImageTitle";
 import Videos from "../access/Videos";
 
 export default function Home() {
@@ -13,31 +13,29 @@ export default function Home() {
           <Slider />
         </div>
       </div>
-      <div className="bg-content h-[400px]">
-        <div className=" container mx-auto drop-shadow-lg ">
-          <div className="  mx-auto px-16">
-            <div className=" grid grid-cols-2 h-[200px] lg:h-[480px] w-full max-w-full xl:h-[320px] items-center">
-              <div className="">
+      <div className="bg-content h-[440px] ">
+        <div className=" container flex mx-auto drop-shadow-lg  ">
+          <div className="  mx-auto px-16 ">
+            <div className=" grid grid-cols-2 my-8 ">
+              <div className=" ">
                 <h1 className="font-sans text-6xl">
                   "Lựa chọn đúng, 
                 </h1>
                 <h1 className="font-sans text-6xl pl-16">
                   thành công gần"
                 </h1>
-                <p className="mt-6 font-sans tracking-wider text-justify first-letter:font-bold first-letter:text-5xl first-letter:float-left">
+                <p className="mt-6 font-sans tracking-wider text-justify italic">
                   Trường Đại học Đà Lạt (Dalat University) là một trường đại học
                   công lập tại Việt Nam, đào tạo đa ngành, đa lĩnh vực, cung cấp
                   nguồn nhân lực trình độ cao về Khoa học tự nhiên và Công nghệ,
-                  Khoa học xã hội và Nhân văn, và Kinh tế
+                  Khoa học xã hội và Nhân văn, và Kinh tế.
                 </p>
-                <button class="mt-6 h-12  w-32 rounded-full bg-primary text-white ring-4 ring-blue-400 drop-shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-400 duration-300">
-                  <a href="/">Read More</a>
-                </button>
+                <HomeButtons title ={"Read More"}/>
               </div>
               <img
                 src={images.introimg}
                 alt="Img"
-                className="flex px-3 mt-5 ml-4 max-w-full  min-w-[300px] h-[300px] rounded-3xl "
+                className=" px-3 mt-5 ml-4 max-w-full  min-w-[300px] h-[300px] rounded-3xl  "
               />
             </div>
           </div>
